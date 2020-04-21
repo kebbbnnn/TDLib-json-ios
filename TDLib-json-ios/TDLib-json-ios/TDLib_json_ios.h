0616a08fb461c1585b9d98f98afbe5d6da0dca78
Kevin Ladan <kevinladan@outlook.com>
2020-04-21 09:11:49 +0800

    Initial Project

---
+++ TDLib-json-ios/TDLib-json-ios/TDLib_json_ios.h
@@ -0,0 +1,19 @@
+//
+//  TDLib_json_ios.h
+//  TDLib-json-ios
+//
+//  Created by Kevin Ladan on 21/4/20.
+//  Copyright © 2020 Kevin Ladan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TDLib_json_ios.
+FOUNDATION_EXPORT double TDLib_json_iosVersionNumber;
+
+//! Project version string for TDLib_json_ios.
+FOUNDATION_EXPORT const unsigned char TDLib_json_iosVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TDLib_json_ios/PublicHeader.h>
+
+
---
+++ TDLib-json-ios/TDLib-json-iosTests/TDLib_json_iosTests.swift
@@ -0,0 +1,34 @@
+//
+//  TDLib_json_iosTests.swift
+//  TDLib-json-iosTests
+//
+//  Created by Kevin Ladan on 21/4/20.
+//  Copyright © 2020 Kevin Ladan. All rights reserved.
+//
+
+import XCTest
+@testable import TDLib_json_ios
+
+class TDLib_json_iosTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
